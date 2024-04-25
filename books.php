5aaf73e42be9d48d5375e0448b2a7338ced065b9
Evan Tierney <ejt7yqz@virginia.edu>
2024-04-24 22:03:36 -0400

    Initial commit

---
+++ books.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2>
+            Books
+        </h2>
+        <h3>List of books</h3>
+        <div class="row justify-content-center">  
+            <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+                <thead>
+                    <tr style="background-color:#B0B0B0">
+                        <th width="30%"><b>ISBN</b></th>
+                        <th width="30%"><b>Title</b></th>        
+                        <th width="30%"><b>SeriesTitle</b></th> 
+                        <th width="30%"><b>PageCount</b></th>
+                        <th width="30%"><b>PubDate</b></th>        
+                        <th><b>Update?</b></th>
+                        <th><b>Delete?</b></th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </body>
+</html>
