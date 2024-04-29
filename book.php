293b4b8020e4a5fc7cc8dcdf13076ac86cb72131
Evan Tierney <ejt7yqz@virginia.edu>
2024-04-29 09:59:20 -0400

    Weird navbar in books.php

--- book.php
+++ book.php
@@ -56,14 +56,15 @@ $ratingInfo = getRatingInfo($isbn);
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
+                    <!-- Navbar is all screwy here -->
+                    <!-- <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="books.php">Browse</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Reading Lists</a>
                     </li>
                 </ul>
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto"> -->
                     <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true): ?>
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="books.php">Browse</a>
